44f47bd6d852869786f66a7b83d428ea059287f8
Venky04064 <venkiaiautomations@gmail.com>
2025-09-25 18:08:46 +0530

    🦸‍♂️ AI HERO FIX: Added Google Fonts to _document.js to fix stylesheet error

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* ✅ HERO FIX: Google Fonts moved to _document.js as required */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=Roboto:wght@300;400;500;700&family=Poppins:wght@300;400;500;600;700&family=Montserrat:wght@300;400;500;600;700&family=Open+Sans:wght@300;400;500;600;700&family=Lato:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+        
         {/* PWA Meta Tags */}
         <meta name="application-name" content="Portfolio by Comet" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
@@ -30,6 +38,10 @@ export default function Document() {
         {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@venkatesh" />
+        
+        {/* Performance Hints */}
+        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
         
         {/* Critical CSS for loading state */}
         <style dangerouslySetInnerHTML={{
